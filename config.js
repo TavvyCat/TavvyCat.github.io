b2a74f6bcea9db2c9aa5157de9e9ba074daa167f
TavvyCat <tavish.cary@welch.edu>
2020-12-01 21:23:01 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,80 @@
+window.PorfolioConfig = {
+  name: 'Tavish James Kingshott Cary',
+  jobTitle: 'Full-stack Software Engineer',
+  jobDescription: 'I am Tavish Cary. I am a software developer with a background in philosophy. While I have no formal education in philosophy, I’ve spent the better part of the last 8 years figuring out the world and how it works. This includes theology, politics, ethics, economics, sociology, and everything in between. This has made me good at figuring out complex problems. One change in logic can create a cascade of changes down the line. I’m good at seeing the flaws in certain ways of thinking and will bring that into my software development career.',
+  skills: [
+    {
+      name: 'HTML',
+      description: 'The most basic language, and the first one many people learn. It is the backbone of web development.'
+    },
+    {
+      name: 'Bootstrap',
+      description: 'I\'m not the most creative or artistic person. So I like using libraries like Bootstrap for styling.'
+    },
+    {
+      name: 'JavaScript',
+      description: 'JS is my favorite language. I love how logical it is and all the cool things you can do with it.'
+    },
+    {
+      name: 'React',
+      description: 'An amazing JS library (or framework depending on who you ask). It\'s my favorite way to build websites. It so easily combines JS and HTML and makes your code into a very dynamic SPA.'
+    },
+    {
+      name: 'Node',
+      description: 'JavaScript for the back end. I like working on the backend with Node and Express. And I get to use JS.'
+    },
+    {
+      name: 'MongoDB',
+      description: 'Mongo is a super fun and simple database, that\'s good for simple requests. Plus, you can use JS with Mongoose.'
+    },
+    {
+      name: 'SQL',
+      description: 'SQL is a great database language. I love the way you can create connected tables.'
+    },
+  ],
+  works: [
+    {
+      name: 'Therapy Nomads Blog',
+      link: 'https://therapynomads.com',
+      description: 'This is a personal blog recording the travels and adventures for my wife and I. It was built primarily with React. It was built almost from scratch. I utilized Google Firebase to host it and use as a back end for storage and database.'
+    },
+    {
+      name: 'Travel Rehab Specialist Job Site',
+      link: 'https://tavvycat.github.io/Travel-Jobs-Client/',
+      description: 'This is an app that connects traveling rehab specialists looking for contracts to facilities and companies offering contracts. When someone signs in they can choose employer or employee, and employees choose which occupation they are. The Employers can post new jobs that anyone can see. And the employees see those jobs (next version will have ability to apply).'
+    },
+    {
+      name: 'Tic Tac Toe Game',
+      link: 'https://tavvycat.github.io/tic-tac-toe-client/',
+      description: 'This is the front end for a simple Tic Tac Toe game where you can sign up for an account and once signed in you can play an infinite amount of games. The API will keep track of all the games you\'ve played. I\'ve also added a computer player that will try to block you when you get 2 in a row.'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Video Games',
+      description: 'I\'ve played video games basically since birth. I think they are a great way to either relax or train your mind in strategy.'
+    },
+    {
+      name: 'Learning',
+      description: 'I love learning about just about everything. Science, Philosophy, etc. I love it all.'
+    },
+    {
+      name: 'Loving My Cats',
+      description: 'I have two kitties named Neko and Zeus. They\'re both black and so cute! Sometimes they play fetch with me. And they sleep with me at night'
+    }
+  ],
+  contacts: {
+    email: 'tavish.cary@welch.edu',
+    mobileNo: '+3132418354',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/TavvyCat'
+      },
+      {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/tavish-cary/'
+      }
+    ]
+  }
+};
